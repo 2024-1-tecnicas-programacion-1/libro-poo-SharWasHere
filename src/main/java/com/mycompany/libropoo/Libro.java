e566d35000a4e8764682fd6ade70209828122408
SharWasHere <159039235+SharWasHere@users.noreply.github.com>
2024-04-22 10:30:47 -0500

    Update Libro.java

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -9,6 +9,10 @@ public class Libro {
     private boolean leido;
 
     public Libro(String titulo, String autor, int anio, String genero) {
+        
+     /**
+     * Complejidad temporal: O(1) Tiempo constante
+     */
 
         this.titulo = titulo;
         this.autor = autor;
@@ -18,20 +22,32 @@ public class Libro {
 
     }
 
+     /**
+     * Complejidad temporal: O(1) Tiempo constante
+     */
     public String mostrarInformacion() {
         String mostrar = titulo + " " +anio + " " +genero;
         return mostrar;
     }
 
+     /**
+     * Complejidad temporal: O(1) Tiempo constante
+     */
     public void marcarLeido() {
         leido = true;
 
     }
+     /**
+     * Complejidad temporal: O(1) Tiempo constante
+     */
     
     public boolean esLeido(){
         return this.leido;
     }
 
+     /**
+     * Complejidad temporal: O(1) Tiempo constante
+     */
     public boolean esAntiguo() {
         int diferencia = 2024 - anio;
 
