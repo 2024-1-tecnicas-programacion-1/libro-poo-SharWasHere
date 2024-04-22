5368b733f901cb325265df92bf06bef0d943c95e
Katherine Ortiz <katherinem.ortizb@konradlorenz.edu.co>
2024-04-22 10:27:35 -0500

    Agrego el taller terminado

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,45 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anio;
+    private boolean leido;
+
+    public Libro(String titulo, String autor, int anio, String genero) {
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anio = anio;
+        this.leido = false;
+
+    }
+
+    public String mostrarInformacion() {
+        String mostrar = titulo + " " +anio + " " +genero;
+        return mostrar;
+    }
+
+    public void marcarLeido() {
+        leido = true;
+
+    }
     
+    public boolean esLeido(){
+        return this.leido;
+    }
+
+    public boolean esAntiguo() {
+        int diferencia = 2024 - anio;
+
+        if (diferencia > 50) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,45 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        System.out.println("indique el titulo");
+        String tituloUsuario = lector.nextLine();
+
+        System.out.println("indique el autor");
+        String autorUsuario = lector.nextLine();
+
+        System.out.println("indique el genero del autor");
+        String generoUsuario = lector.nextLine();
+
+        System.out.println("indique el año de publicación");
+        int anioUsuario = lector.nextInt();
+
+        
+        Libro usuarioLibro = new Libro(tituloUsuario, autorUsuario, anioUsuario, generoUsuario);
+        
+        System.out.println(usuarioLibro.mostrarInformacion());
+        
+        usuarioLibro.marcarLeido();
+        
+        if(usuarioLibro.esLeido()){
+            System.out.println("Ya fue leídoi");
+            
+        }else{
+            System.out.println("No fue leído");
+        }
+        
+        if(usuarioLibro.esAntiguo()){
+            System.out.println("El libro es antiguo");
+        }else{
+            System.out.println("El libro no es antiguo");
+        }
+    }
+
     
+
 }
